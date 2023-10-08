3bc6eb88dcd4dfe8e9f9071a642965d1323b29c1
Mart510 <martynandrewbillings@gmail.com>
2023-10-08 19:51:07 +0100

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+// Import leaflet
+import * as L from 'leaflet';
+
 // Initialize a Leaflet map
 const myMap = L.map('map').setView([51.505, -0.09], 13);
-
 
 // Add basic tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
